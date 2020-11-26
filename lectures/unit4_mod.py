501419c70e3b43d260343a478e0c8a957f9e65a7
Richard Foltyn <richard.foltyn@gmail.com>
2020-11-26 21:27:52 +0000

    Unit 4: Varios fixes and additions to notebook

---
+++ lectures/unit4_mod.py
@@ -0,0 +1,11 @@
+"""
+Module unit4_mod used to demonstrate the usage of modules in unit 4.
+"""
+
+# global variable in this module
+var = 1
+
+
+# global function in this module
+def func():
+    print('func in module unit4_mod called')
