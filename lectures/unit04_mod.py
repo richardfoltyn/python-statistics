55dd9f49a266fa75630ffa82992a95790e3ed8db
Richard Foltyn <richard.foltyn@gmail.com>
2020-12-18 17:52:46 +0000

    Rename all units to have leading zeros

---
+++ lectures/unit04_mod.py
@@ -0,0 +1,11 @@
+"""
+Module unit4_mod used to demonstrate the usage of modules in unit 4.
+"""
+
+# global variable in this module
+var = 'Variable defined in unit04_mod'
+
+
+# global function in this module
+def func():
+    print('func in module unit04_mod called')
